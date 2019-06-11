0b5924b685508c6d9407c3a279f5dccb57977574
iosdevlog <iosdevlog@iosdevlog.com>
2019-06-11 14:05:33 +0800

    chore(release): v0.2.3
    import kmeans

--- slmethod/__init__.py
+++ slmethod/__init__.py
@@ -23,7 +23,7 @@ name = "slmethod"
 # Dev branch marker is: "X.Y.dev" or "X.Y.devN" where N is an integer.
 # "X.Y.dev0" is the canonical version of "X.Y.dev"
 #
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 try:
     # 此变量是由生成过程在 _builtins_ 中注入的。它用于在未生成二进制文件时启用 slmethod 子包的导入
@@ -36,4 +36,4 @@ if __SKLEARN_SETUP__:
 else:
     from .utils._show_versions import show_versions
 
-    __all__ = ["perceptron"]
+    __all__ = ["perceptron", "kmeans"]
